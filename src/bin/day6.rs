59991b841f6e0853f571435c6556d451c3d73ff2
jsgroth <1137683+jsgroth@users.noreply.github.com>
2024-12-06 00:20:02 -0600

    (day6) better solution, part 2 runtime down from 300 milliseconds to 14-15 milliseconds
    instead of individually checking every position, only check empty spaces that the guard is about to step into, and take advantage of recursion to avoid needing to recreate the guard's path up to that point

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,6 +2,7 @@
 //!
 //! <https://adventofcode.com/2024/day/6>
 
+use rustc_hash::FxHashSet;
 use std::error::Error;
 
 #[derive(Debug, Clone, Copy)]
@@ -74,79 +75,118 @@ fn parse_input(input: &str) -> Input {
 
 fn solve_part_1(input: &str) -> usize {
     let Input { map, guard_start } = parse_input(input);
-
-    let TraverseResult::Escaped { visited } = traverse_map(&map, guard_start) else {
-        panic!("Failed to escape the map");
-    };
-    visited
+    traverse_map(&map, guard_start)
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum TraverseResult {
-    Escaped { visited: usize },
-    Looped,
-}
-
-fn traverse_map(map: &[Vec<Space>], start: (i32, i32)) -> TraverseResult {
-    let mut visited = vec![vec![0_u8; map[0].len()]; map.len()];
+fn traverse_map(map: &[Vec<Space>], start: (i32, i32)) -> usize {
+    let mut visited = vec![vec![false; map[0].len()]; map.len()];
 
     let (mut row, mut col) = start;
     let mut direction = Direction::Up;
-    while (0..map.len() as i32).contains(&row) && (0..map[0].len() as i32).contains(&col) {
-        if visited[row as usize][col as usize] & (direction as u8) != 0 {
-            return TraverseResult::Looped;
-        }
-        visited[row as usize][col as usize] |= direction as u8;
+    loop {
+        visited[row as usize][col as usize] = true;
 
         let (next_row, next_col) = direction.step(row, col);
-        if (0..map.len() as i32).contains(&next_row)
-            && (0..map[0].len() as i32).contains(&next_col)
-            && map[next_row as usize][next_col as usize] == Space::Obstacle
+        if !(0..map.len() as i32).contains(&next_row)
+            || !(0..map[0].len() as i32).contains(&next_col)
         {
+            break;
+        }
+
+        if map[next_row as usize][next_col as usize] == Space::Obstacle {
             direction = direction.rotate_right();
         } else {
             (row, col) = (next_row, next_col);
         }
     }
 
-    let visited_count = visited
+    visited
         .into_iter()
-        .map(|row| {
-            row.into_iter()
-                .filter(|&directions| directions != 0)
-                .count()
-        })
-        .sum();
-
-    TraverseResult::Escaped {
-        visited: visited_count,
-    }
+        .map(|row| row.into_iter().filter(|&b| b).count())
+        .sum()
 }
 
-fn solve_part_2(input: &str) -> u32 {
+fn solve_part_2(input: &str) -> usize {
     let Input {
         mut map,
         guard_start,
     } = parse_input(input);
 
-    let mut loop_positions = 0;
-    for test_row in 0..map.len() {
-        for test_col in 0..map[0].len() {
-            if map[test_row][test_col] == Space::Obstacle
-                || (test_row as i32, test_col as i32) == guard_start
-            {
-                continue;
-            }
+    let mut visited = vec![vec![0; map[0].len()]; map.len()];
+    let mut result = FxHashSet::default();
+    traverse_part_2(
+        &mut map,
+        &mut visited,
+        guard_start,
+        Direction::Up,
+        None,
+        &mut result,
+    );
 
-            map[test_row][test_col] = Space::Obstacle;
-            if traverse_map(&map, guard_start) == TraverseResult::Looped {
-                loop_positions += 1;
+    result.len()
+}
+
+fn traverse_part_2(
+    map: &mut [Vec<Space>],
+    visited: &mut Vec<Vec<u8>>,
+    (mut row, mut col): (i32, i32),
+    mut direction: Direction,
+    obstacle_location: Option<(i32, i32)>,
+    result: &mut FxHashSet<(i32, i32)>,
+) {
+    let mut visits = Vec::new();
+
+    loop {
+        if visited[row as usize][col as usize] & (direction as u8) != 0 {
+            result.insert(obstacle_location.unwrap());
+            break;
+        }
+        visited[row as usize][col as usize] |= direction as u8;
+        visits.push((row, col, direction));
+
+        let (next_row, next_col) = direction.step(row, col);
+        if !(0..map.len() as i32).contains(&next_row)
+            || !(0..map[0].len() as i32).contains(&next_col)
+        {
+            // Went out of bounds
+            break;
+        }
+
+        if map[next_row as usize][next_col as usize] == Space::Obstacle {
+            // Ran into an obstacle; rotate
+            direction = direction.rotate_right();
+        } else {
+            if obstacle_location.is_none()
+                && visited[next_row as usize][next_col as usize] == 0
+                && !result.contains(&(next_row, next_col))
+            {
+                // No obstacle has been inserted yet, and the space ahead is:
+                //   * Empty
+                //   * Has not been visited yet
+                //   * Is not already part of the result (i.e. an obstacle was placed there and caused a loop)
+                // Insert the obstacle, recurse, then remove the obstacle
+                map[next_row as usize][next_col as usize] = Space::Obstacle;
+                traverse_part_2(
+                    map,
+                    visited,
+                    (row, col),
+                    direction.rotate_right(),
+                    Some((next_row, next_col)),
+                    result,
+                );
+                map[next_row as usize][next_col as usize] = Space::Empty;
             }
-            map[test_row][test_col] = Space::Empty;
+            (row, col) = (next_row, next_col);
         }
     }
 
-    loop_positions
+    unwind_visits(visited, &visits);
+}
+
+fn unwind_visits(visited: &mut [Vec<u8>], visits: &[(i32, i32, Direction)]) {
+    for &(row, col, direction) in visits {
+        visited[row as usize][col as usize] &= !(direction as u8);
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
