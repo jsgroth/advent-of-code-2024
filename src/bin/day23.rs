cafa7ed9d2c84b01461b87da0353d80311ffafee
jsgroth <1137683+jsgroth@users.noreply.github.com>
2024-12-23 16:10:33 -0600

    (day23) significantly faster Part 2 solution; down from 265-270ms to 9-10ms
    still a fairly brute force solution, but much smarter about optimizing the search space, and converting all computer strings to u32s before doing the search for much faster hashing

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -4,6 +4,7 @@
 
 use rustc_hash::{FxHashMap, FxHashSet};
 use std::error::Error;
+use std::iter;
 
 fn parse_input(input: &str) -> Vec<(&str, &str)> {
     input
@@ -62,36 +63,80 @@ fn build_connections_map<'a>(
 
 fn solve_part_2(input: &str) -> String {
     let connections = parse_input(input);
-    let connections_set = build_connections_set(&connections);
-    let connections_map = build_connections_map(&connections);
-    let computers: Vec<_> = connections_map.keys().copied().collect();
 
-    let mut max = max_group(&computers, &connections_set, &[]);
-    max.sort();
+    // Accumulate all unique computer strings into a Vec
+    let computers: FxHashSet<_> = connections.iter().flat_map(|&(a, b)| [a, b]).collect();
+    let computers: Vec<_> = computers.into_iter().collect();
 
-    max.join(",")
-}
+    // Convert everything to u32s because that is significantly faster
+    // Replace each computer string with its index in `computers`
+    let computer_idx_map: FxHashMap<&str, u32> =
+        computers.iter().enumerate().map(|(i, &computer)| (computer, i as u32)).collect();
 
-fn max_group<'a>(
-    computers: &[&'a str],
-    connections: &FxHashSet<(&str, &str)>,
-    group: &[&'a str],
-) -> Vec<&'a str> {
-    if computers.is_empty() {
-        return group.to_vec();
-    }
+    let mut connections_map: FxHashMap<u32, Vec<u32>> = FxHashMap::default();
+    let mut connections_set: FxHashSet<(u32, u32)> = FxHashSet::default();
+    for &(a, b) in &connections {
+        let a_idx = *computer_idx_map.get(&a).unwrap();
+        let b_idx = *computer_idx_map.get(&b).unwrap();
 
-    let mut max = max_group(&computers[1..], connections, group);
-    if group.iter().all(|computer| connections.contains(&(computer, computers[0]))) {
-        let mut new_group = group.to_vec();
-        new_group.push(computers[0]);
-        let sub_result = max_group(&computers[1..], connections, &new_group);
-        if sub_result.len() > max.len() {
-            max = sub_result;
+        for (aa, bb) in [(a_idx, b_idx), (b_idx, a_idx)] {
+            connections_map.entry(aa).or_default().push(bb);
+            connections_set.insert((aa, bb));
         }
     }
 
-    max
+    // Sort the connections map values in reverse order to make it possible to avoid needing to
+    // scan the entire Vec later
+    for value in connections_map.values_mut() {
+        value.sort_by(|a, b| a.cmp(b).reverse());
+    }
+
+    let max_group = find_max_group(computers.len() as u32, &connections_map, &connections_set);
+    let mut max_group_str: Vec<_> =
+        max_group.into_iter().map(|idx| computers[idx as usize]).collect();
+    max_group_str.sort();
+
+    max_group_str.join(",")
+}
+
+fn find_max_group(
+    num_computers: u32,
+    connections_map: &FxHashMap<u32, Vec<u32>>,
+    connections_set: &FxHashSet<(u32, u32)>,
+) -> Vec<u32> {
+    // Initialize with a single group for each computer
+    let mut groups: Vec<_> = (0..num_computers).map(|computer| vec![computer]).collect();
+
+    // Loop until there is only 1 group left
+    // In each iteration, replace `groups` with all groups that are 1 larger
+    let mut solution = Vec::new();
+    while !groups.is_empty() {
+        let mut next_groups = Vec::new();
+
+        // This silliness is necessary because consuming `groups` in the following loop slightly
+        // improves performance compared to not consuming it (i.e. `for group in &groups`)
+        if groups.len() == 1 {
+            solution = groups[0].clone();
+        }
+
+        for group in groups {
+            let last = *group.last().unwrap();
+            for &connection in connections_map.get(&group[0]).unwrap() {
+                if connection <= last {
+                    break;
+                }
+
+                if group[1..].iter().all(|&other| connections_set.contains(&(other, connection))) {
+                    next_groups.push(group.iter().copied().chain(iter::once(connection)).collect());
+                }
+            }
+        }
+
+        groups = next_groups;
+    }
+
+    assert!(!solution.is_empty(), "More than 1 group of max length");
+    solution
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
